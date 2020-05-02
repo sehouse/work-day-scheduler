1e2febe3f1694761bdbb8543d0e54f79f58e0f8c
sehouse <sehouse@umich.edu>
2020-05-02 17:00:04 -0400

    Removed console logs for working functions and changed the format of the displayed date and time, removing seconds.

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 $(document).ready(function () {
-  console.log("page is loaded");
-
   //Dom manipulatio to put the current date into the HTML header when the page is loaded, using moment.js
 
-  var currentTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss A");
-
+  var currentTime = moment().format("dddd, MMMM Do, YYYY, h:mm A");
   var currentDate = $("#currentDate");
-
   currentDate.text(currentTime);
 });
 
@@ -19,8 +15,6 @@ $("#schedule1").val(localStorage.getItem("9am"));
 
 function updateSchedule1() {
   localStorage.setItem("9am", $("#schedule1").val());
-
-  console.log(localStorage);
 }
 
 //2
@@ -30,8 +24,6 @@ $("#schedule2").val(localStorage.getItem("10am"));
 
 function updateSchedule2() {
   localStorage.setItem("10am", $("#schedule2").val());
-
-  console.log(localStorage);
 }
 
 //3
@@ -41,8 +33,6 @@ $("#schedule3").val(localStorage.getItem("11am"));
 
 function updateSchedule3() {
   localStorage.setItem("11am", $("#schedule3").val());
-
-  console.log(localStorage);
 }
 
 //4
@@ -52,8 +42,6 @@ $("#schedule4").val(localStorage.getItem("12pm"));
 
 function updateSchedule4() {
   localStorage.setItem("12pm", $("#schedule4").val());
-
-  console.log(localStorage);
 }
 
 //5
@@ -63,8 +51,6 @@ $("#schedule5").val(localStorage.getItem("1pm"));
 
 function updateSchedule5() {
   localStorage.setItem("1pm", $("#schedule5").val());
-
-  console.log(localStorage);
 }
 
 //6
@@ -74,8 +60,6 @@ $("#schedule6").val(localStorage.getItem("2pm"));
 
 function updateSchedule6() {
   localStorage.setItem("2pm", $("#schedule6").val());
-
-  console.log(localStorage);
 }
 
 //7
@@ -85,8 +69,6 @@ $("#schedule7").val(localStorage.getItem("3pm"));
 
 function updateSchedule7() {
   localStorage.setItem("3pm", $("#schedule7").val());
-
-  console.log(localStorage);
 }
 
 //8
@@ -96,8 +78,6 @@ $("#schedule8").val(localStorage.getItem("4pm"));
 
 function updateSchedule8() {
   localStorage.setItem("4pm", $("#schedule8").val());
-
-  console.log(localStorage);
 }
 
 //9
@@ -107,14 +87,18 @@ $("#schedule9").val(localStorage.getItem("5pm"));
 
 function updateSchedule9() {
   localStorage.setItem("5pm", $("#schedule9").val());
-
-  console.log(localStorage);
 }
 
 //checks for time to change color of the rows
 
 //A function to clear local storage and refresh page
+
 $("#clear").on("click", clear);
+
 function clear() {
   localStorage.clear();
+
+  //reload function found through https://www.w3schools.com/jsref/met_loc_reload.asp
+
+  location.reload();
 }
