da8d2222572db567670e12a0709abffd19400db1
sehouse <sehouse@umich.edu>
2020-05-02 14:34:47 -0400

    Added pseudocode to try to map out the steps for creation of the planner.

---
+++ script.js
@@ -0,0 +1,25 @@
+//A function to put the current date into the HTML header when the page is loaded, using moment.js
+
+//A function to clear local storage and refresh page
+
+// 9 functions to save the user's text input for each row into local storage
+
+//1
+
+//2
+
+//3
+
+//4
+
+//5
+
+//6
+
+//7
+
+//8
+
+//9
+
+//checks for time to change color of the rows
